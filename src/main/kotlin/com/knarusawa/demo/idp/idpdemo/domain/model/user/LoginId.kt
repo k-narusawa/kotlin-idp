2b94430ad149d72a5632f6d366bbdb9d95df2a35
k-narusawa <swkei320@gmail.com>
2023-07-08 20:43:51 +0900

    refactor: 💡 toString()の返し方を修正

--- src/main/kotlin/com/knarusawa/demo/idp/idpdemo/domain/model/user/LoginId.kt
+++ src/main/kotlin/com/knarusawa/demo/idp/idpdemo/domain/model/user/LoginId.kt
@@ -3,7 +3,5 @@ package com.knarusawa.demo.idp.idpdemo.domain.model.user
 data class LoginId(
   val value: String
 ) {
-  override fun toString(): String {
-    return value
-  }
+  override fun toString() = value
 }
--- src/main/kotlin/com/knarusawa/demo/idp/idpdemo/domain/model/user/Password.kt
+++ src/main/kotlin/com/knarusawa/demo/idp/idpdemo/domain/model/user/Password.kt
@@ -3,7 +3,5 @@ package com.knarusawa.demo.idp.idpdemo.domain.model.user
 data class Password(
   val value: String?
 ) {
-  override fun toString(): String {
-    return value ?: ""
-  }
+  override fun toString() = value ?: ""
 }
