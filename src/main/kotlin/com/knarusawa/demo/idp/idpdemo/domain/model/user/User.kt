055ed4575f2f666401d943a6cc3ffee6d0fe82e1
k-narusawa <narusawakohei@team-lab.com>
2023-08-22 19:28:31 +0900

    wip: 🚧 ドメインイベントを途中まで実装

--- src/main/kotlin/com/knarusawa/demo/idp/idpdemo/domain/model/user/User.kt
+++ src/main/kotlin/com/knarusawa/demo/idp/idpdemo/domain/model/user/User.kt
@@ -2,6 +2,7 @@ package com.knarusawa.demo.idp.idpdemo.domain.model.user
 
 import com.knarusawa.demo.idp.idpdemo.configuration.SecurityConfig
 import com.knarusawa.demo.idp.idpdemo.infrastructure.db.record.UserRecord
+import org.springframework.data.domain.AbstractAggregateRoot
 import java.time.LocalDateTime
 
 class User private constructor(
@@ -13,7 +14,7 @@ class User private constructor(
   failedAttempts: Int?,
   lockTime: LocalDateTime?,
   isDisabled: Boolean,
-) {
+) : AbstractAggregateRoot<User>() {
   var loginId: LoginId = loginId
     private set
   var password: Password = password
@@ -61,5 +62,6 @@ class User private constructor(
 
   fun updatePassword(password: String) {
     this.password = Password(value = SecurityConfig().passwordEncoder().encode(password))
+    registerEvent(UserEvent.UpdateEvent())
   }
 }
