a3246a9339f9cb04d040e1cb4d633326958c0009
k-narusawa <swkei320@gmail.com>
2023-08-26 08:31:17 +0900

    refactor: 💡 不要になったユースケースを削除

--- src/main/kotlin/com/knarusawa/demo/idp/idpdemo/application/service/user/getAll/UserGetAllOutputData.kt
+++
@@ -1,7 +0,0 @@
-package com.knarusawa.demo.idp.idpdemo.application.service.user.getAll
-
-import com.knarusawa.demo.idp.idpdemo.domain.model.user.UserReadModel
-
-data class UserGetAllOutputData(
-  val users: List<UserReadModel>
-)
