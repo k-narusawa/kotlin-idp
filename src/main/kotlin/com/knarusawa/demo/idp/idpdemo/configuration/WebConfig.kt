a78ab5ce88427555faecc5d3197d9314ba782c29
k-narusawa <swkei320@gmail.com>
2023-08-29 21:37:31 +0900

    fix: 🐛 filterのConfigへBean登録処理が不要だったので削除

--- src/main/kotlin/com/knarusawa/demo/idp/idpdemo/configuration/WebConfig.kt
+++ src/main/kotlin/com/knarusawa/demo/idp/idpdemo/configuration/WebConfig.kt
@@ -1,18 +1,7 @@
 package com.knarusawa.demo.idp.idpdemo.configuration
 
-import com.knarusawa.demo.idp.idpdemo.infrastructure.filter.RequestFilter
-import org.springframework.boot.web.servlet.FilterRegistrationBean
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
-  @Bean
-  fun filterRegister(): FilterRegistrationBean<RequestFilter> {
-    val registrationBean = FilterRegistrationBean<RequestFilter>()
-    registrationBean.filter = RequestFilter()
-    registrationBean.addUrlPatterns("/*")
-    return registrationBean
-  }
-}
+class WebConfig : WebMvcConfigurer
