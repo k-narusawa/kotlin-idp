440a8bbd4e986eaae4908b7bf1d0ab386698ae5d
k-narusawa <swkei320@gmail.com>
2023-08-29 21:38:10 +0900

    feat: 🎸 リクエストのログを出力する
    filterで実装

--- src/main/kotlin/com/knarusawa/demo/idp/idpdemo/infrastructure/filter/RequestFilter.kt
+++ src/main/kotlin/com/knarusawa/demo/idp/idpdemo/infrastructure/filter/RequestFilter.kt
@@ -9,14 +9,18 @@ import org.springframework.web.filter.OncePerRequestFilter
 
 @Component
 class RequestFilter : OncePerRequestFilter() {
-  override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
-    logger.info("request User-Agent: " + request.getHeader("User-Agent"));
+  override fun doFilterInternal(
+    request: HttpServletRequest,
+    response: HttpServletResponse,
+    filterChain: FilterChain
+  ) {
+    logger.debug("request User-Agent: " + request.getHeader("User-Agent"));
     val start = System.currentTimeMillis()
     try {
       filterChain.doFilter(request, response)
     } finally {
       val end = System.currentTimeMillis()
-      logger.info("METHOD: ${request.method}, REQUEST_URL: ${request.requestURI}, REQUEST_TIME: ${end - start}ms")
+      logger.info("METHOD: ${request.method}, REQUEST_URL: ${request.requestURI}, RESPONSE_TIME: ${end - start}ms")
     }
   }
 }
