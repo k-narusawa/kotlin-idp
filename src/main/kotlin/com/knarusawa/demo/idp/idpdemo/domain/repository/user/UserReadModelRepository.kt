46b236a197dc9d12678dfba76ad2c9c8a6fe71f7
k-narusawa <swkei320@gmail.com>
2023-08-31 21:22:25 +0900

    refactor: 💡 不要なメソッドを削除

--- src/main/kotlin/com/knarusawa/demo/idp/idpdemo/domain/repository/user/UserReadModelRepository.kt
+++ src/main/kotlin/com/knarusawa/demo/idp/idpdemo/domain/repository/user/UserReadModelRepository.kt
@@ -4,6 +4,4 @@ import com.knarusawa.demo.idp.idpdemo.domain.model.user.UserReadModel
 
 interface UserReadModelRepository {
   fun findByLoginId(loginId: String): UserReadModel?
-  fun findByUserId(userId: String): UserReadModel?
-  fun findAll(): List<UserReadModel>
 }
