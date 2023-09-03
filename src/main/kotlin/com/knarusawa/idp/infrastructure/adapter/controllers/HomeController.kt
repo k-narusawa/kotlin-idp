bc16de733d6151fd45f5f926f0c9b8654f4f4c12
k-narusawa <swkei320@gmail.com>
2023-09-03 20:52:37 +0900

    feat: 🎸 メアドを画面に表示させる

--- src/main/kotlin/com/knarusawa/idp/infrastructure/adapter/controllers/HomeController.kt
+++ src/main/kotlin/com/knarusawa/idp/infrastructure/adapter/controllers/HomeController.kt
@@ -2,16 +2,20 @@ package com.knarusawa.idp.infrastructure.adapter.controllers
 
 import com.knarusawa.idp.application.service.UserActivityDtoQueryService
 import com.knarusawa.idp.application.service.UserDtoQueryService
+import com.knarusawa.idp.application.service.UserMailDtoQueryService
 import java.security.Principal
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
+@PreAuthorize("hasRole('USER')")
 class HomeController(
   private val userDtoQueryService: UserDtoQueryService,
+  private val userMailDtoQueryService: UserMailDtoQueryService,
   private val userActivityDtoQueryService: UserActivityDtoQueryService,
 ) {
   @GetMapping("/")
@@ -21,7 +25,10 @@ class HomeController(
       userId = principal.name,
       pageable = PageRequest.of(0, 3, Sort.by("timestamp").descending())
     )
+    val userMail = userMailDtoQueryService.findByUserId(userId = principal.name)
+
     model.addAttribute("user", user)
+    model.addAttribute("userMail", userMail)
     model.addAttribute("userActivities", userActivities)
     return "index"
   }
