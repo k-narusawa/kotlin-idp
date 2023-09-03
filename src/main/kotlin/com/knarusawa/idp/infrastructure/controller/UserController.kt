84cbf494bc227419b08ea49ffa978320edeb1e4a
k-narusawa <swkei320@gmail.com>
2023-09-03 11:38:40 +0900

    feat: 🎸 行動履歴詳細画面の作成

--- src/main/kotlin/com/knarusawa/idp/infrastructure/controller/UserController.kt
+++ src/main/kotlin/com/knarusawa/idp/infrastructure/controller/UserController.kt
@@ -1,5 +1,6 @@
 package com.knarusawa.idp.infrastructure.controller
 
+import com.knarusawa.idp.application.service.UserActivityDtoQueryService
 import com.knarusawa.idp.application.service.UserDtoQueryService
 import com.knarusawa.idp.application.service.changeUserLoginId.UserLoginIdChangeInputData
 import com.knarusawa.idp.application.service.changeUserLoginId.UserLoginIdChangeService
@@ -10,6 +11,8 @@ import com.knarusawa.idp.domain.model.error.IdpAppException
 import com.knarusawa.idp.infrastructure.dto.ChangeUserLoginIdForm
 import com.knarusawa.idp.infrastructure.dto.ChangeUserPasswordForm
 import java.security.Principal
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -17,15 +20,32 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("user")
 @PreAuthorize("hasRole('USER')")
 class UserController(
   private val userDtoQueryService: UserDtoQueryService,
+  private val userActivityDtoQueryService: UserActivityDtoQueryService,
   private val userLoginIdChangeService: UserLoginIdChangeService,
   private val userPasswordChangeService: UserPasswordChangeService
 ) {
+  @GetMapping("/activities")
+  fun getProfile(
+    model: Model,
+    principal: Principal,
+    @RequestParam page: Int?
+  ): String {
+    val userActivities = userActivityDtoQueryService.findByUserId(
+      userId = principal.name,
+      pageable = PageRequest.of(page ?: 0, 10, Sort.by("timestamp").descending())
+    )
+    model.addAttribute("userActivities", userActivities)
+    model.addAttribute("totalPages", userActivities.totalPages)
+    return "user_activity_list"
+  }
+
   @GetMapping("/login_id/change")
   fun changeLoginId(
     model: Model,
