64d3b19b2542567cf776cf2addd7348f1f15efc4
k-narusawa <swkei320@gmail.com>
2023-09-15 22:41:26 +0900

    refactor: 💡 メアドの形式を修正

--- src/main/kotlin/com/knarusawa/idp/domain/model/user/LoginId.kt
+++ src/main/kotlin/com/knarusawa/idp/domain/model/user/LoginId.kt
@@ -4,7 +4,11 @@ data class LoginId(
   val value: String
 ) {
   init {
-    if (!value.matches(Regex("^[a-zA-Z0-9.!#\$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*\$")))
+    // RFC5322で定義されているメールアドレス形式に準拠
+    val regex =
+      Regex("^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))\$")
+    
+    if (!value.matches(regex))
       throw IllegalArgumentException("メールアドレス形式でない")
   }
 
