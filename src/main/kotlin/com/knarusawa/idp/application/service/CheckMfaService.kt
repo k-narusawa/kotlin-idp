65aa3614a49a7efe6e885ac41032723258fc3b45
k-narusawa <swkei320@gmail.com>
2023-09-15 21:56:13 +0900

    refactor: 💡 OTPの検証処理を修正

--- src/main/kotlin/com/knarusawa/idp/application/service/CheckMfaService.kt
+++ src/main/kotlin/com/knarusawa/idp/application/service/CheckMfaService.kt
@@ -1,10 +1,10 @@
 package com.knarusawa.idp.application.service
 
+import com.knarusawa.idp.domain.model.oneTimePassword.Code
 import com.knarusawa.idp.domain.model.user.UserId
 import com.knarusawa.idp.domain.repository.OnetimePasswordRepository
 import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
-import java.time.LocalDateTime
 
 @Service
 class CheckMfaService(
@@ -15,14 +15,11 @@ class CheckMfaService(
       runBlocking { oneTimePasswordRepository.findByUserId(userId = UserId(userId)) }
         ?: return false
 
-    if (oneTimePassword.expired.isBefore(LocalDateTime.now()))
-      return false
-
-    if (oneTimePassword.code.toString() != otp)
+    if (!oneTimePassword.verify(inputCode = Code(otp)))
       return false
 
     runBlocking { oneTimePasswordRepository.deleteByUserId(userId = UserId(userId)) }
-    
+
     return true
   }
 }
