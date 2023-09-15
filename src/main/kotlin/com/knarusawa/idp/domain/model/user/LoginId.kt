0278139e923a01d5f1926ae43a548e8e80909dd5
k-narusawa <swkei320@gmail.com>
2023-09-15 22:36:16 +0900

    refactor: 💡 ログインIDをメアド形式に修正

--- src/main/kotlin/com/knarusawa/idp/domain/model/user/LoginId.kt
+++ src/main/kotlin/com/knarusawa/idp/domain/model/user/LoginId.kt
@@ -3,5 +3,10 @@ package com.knarusawa.idp.domain.model.user
 data class LoginId(
   val value: String
 ) {
+  init {
+    if (!value.matches(Regex("^[a-zA-Z0-9.!#\$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*\$")))
+      throw IllegalArgumentException("メールアドレス形式でない")
+  }
+
   override fun toString() = value
 }
