4191d8d752f144e5e44efad9c0719ee15c86e31d
k-narusawa <narusawakohei@team-lab.com>
2023-09-29 16:41:40 +0900

    refactor: 💡 クライアント登録部分を修正

--- src/main/kotlin/com/knarusawa/idp/application/service/registerClient/ClientRegisterService.kt
+++ src/main/kotlin/com/knarusawa/idp/application/service/registerClient/ClientRegisterService.kt
@@ -3,12 +3,15 @@ package com.knarusawa.idp.application.service.registerClient
 import com.knarusawa.idp.domain.model.client.Client
 import com.knarusawa.idp.infrastructure.adapter.db.repository.ExtendedRegisteredClientRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ClientRegisterService(
   private val extendedRegisteredClientRepository: ExtendedRegisteredClientRepository
 ) {
+  @Transactional
   fun execute(input: ClientRegisterInputData) {
+    println(input)
     val client = Client.of(
       clientId = input.clientId,
       clientSecret = input.clientSecret,
