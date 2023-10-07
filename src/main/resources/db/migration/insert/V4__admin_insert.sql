3df4783696c040c91cad1c6aac10bd6294c68f7a
k-narusawa <swkei320@gmail.com>
2023-10-07 12:58:01 +0900

    refactor: 💡 スキーマを変更

--- src/main/resources/db/migration/insert/V4__admin_insert.sql
+++ src/main/resources/db/migration/insert/V4__admin_insert.sql
@@ -1,20 +1,24 @@
-INSERT INTO `user` (`user_id`,
-                    `login_id`,
-                    `password`,
-                    `roles`,
-                    `is_lock`,
-                    `failed_attempts`,
-                    `lock_time`,
-                    `is_disabled`,
-                    `created_at`,
-                    `updated_at`)
-VALUES ('8ad115ea-20da-4785-b708-587584b9fef5',
-        'admin@example.com',
-        '$2a$10$2LQvgMLq3WZJTQxRIYAanu3V7EMnQzj.mP5KsI8ruSDTeL0c92gta',
-        'ADMIN,USER',
-        0,
-        0,
-        NULL,
-        0,
-        '2023-01-01 00:00:00',
-        '2023-01-01 00:00:00');
+INSERT INTO `user` (
+  `user_id`,
+  `login_id`,
+  `password`,
+  `roles`,
+  `is_lock`,
+  `failed_attempts`,
+  `lock_time`,
+  `is_disabled`,
+  `created_at`,
+  `updated_at`
+)
+VALUES(
+	'a1565b5b-b0b4-47c2-972f-03cc9e706d3c',
+	'4ko9i+Zj2tXT4hIhlDdD4ONp0Zx/ZH97weP5xkCpInY=',
+	'64o+GjH4rYsh54zWn7+ACvu0RK2J9dGR6boeAUbzyEC0bVe/3I+SyP19dsa5CTfI0+hp3ZuTdpc4PAKTPIKoLQ==',
+	'ADMIN,USER',
+	0,
+	0,
+	NULL,
+	0,
+	'2023-01-01 00:00:00',
+	'2023-01-01 00:00:00'
+);
